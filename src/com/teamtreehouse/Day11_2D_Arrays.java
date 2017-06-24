54898af84ba2b656bc482270989c750edb586820
Tim Neville <nikefootbag@gmail.com>
2017-06-24 23:08:58 +1000

    Initial commit

---
+++ src/com/teamtreehouse/Day11_2D_Arrays.java
@@ -0,0 +1,60 @@
+package com.teamtreehouse;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Created by timneville on 24/6/17.
+ */
+public class Day11_2D_Arrays {
+    public static int arr[][];
+
+    public static int hourGlass(int row, int column) {
+
+        int currentHourGlassSum = 0;
+
+        for (int j = 0; j < 3; j++) {
+            currentHourGlassSum += arr[row][column + j];
+        }
+
+        currentHourGlassSum += arr[row + 1][column + 1];
+
+        for (int j = 0; j < 3; j++) {
+            currentHourGlassSum += arr[row + 2][column + j];
+        }
+
+        return currentHourGlassSum;
+    }
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            arr = new int[][]
+            {
+                {1,1,1,0,0,0},
+                {0,1,0,0,0,0},
+                {1,1,1,0,0,0},
+                {0,0,2,4,4,0},
+                {0,0,0,2,0,0},
+                {0,0,1,2,4,0},
+                {0,0,1,2,4,0},
+                {0,0,1,2,4,0}
+            };
+
+            //in case of negative edge cases
+            int largestHourGlassSum = -1000;
+
+            for (int row = 0; row < arr[0].length-2; row++) {
+                for (int column = 0; column < arr[1].length-2; column++) {
+
+                    largestHourGlassSum = Math.max(largestHourGlassSum, hourGlass(row, column));
+                }
+            }
+            System.out.println(largestHourGlassSum);
+        }
+}
---
+++ src/com/teamtreehouse/Day12_Inheritance.java
@@ -0,0 +1,80 @@
+package com.teamtreehouse;
+
+/**
+ * Created by timneville on 24/6/17.
+ */
+import java.util.*;
+
+public class Day12_Inheritance {
+
+    class Person {
+        protected String firstName;
+        protected String lastName;
+        protected int idNumber;
+
+        // Constructor
+        Person(String firstName, String lastName, int identification){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.idNumber = identification;
+        }
+
+        // Print person data
+        public void printPerson(){
+            System.out.println(
+                    "Name: " + lastName + ", " + firstName
+                            + 	"\nID: " + idNumber);
+        }
+
+    }
+
+    class Student extends Person{
+        private int[] testScores;
+
+    /*
+    *   Class Constructor
+    *
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+        // Write your constructor here
+
+        public Student(String firstName, String lastName, int identification, int[] testScores) {
+            super(firstName, lastName, identification);
+            this.testScores = testScores;
+        }
+
+    /*
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+        // Write your method here
+        public char calculate() {
+            int averageScore = 0;
+
+            for (int each : testScores)
+                averageScore += each;
+
+            averageScore/=testScores.length;
+
+            if (averageScore >= 90)
+                return 'O';
+            else
+            if (averageScore >= 80)
+                return 'E';
+            else
+            if (averageScore >= 70)
+                return 'A';
+            else
+            if (averageScore >= 55)
+                return 'P';
+            else
+            if (averageScore >= 40)
+                return 'D';
+            else
+            return 'T';
+        }
+    }
+}
