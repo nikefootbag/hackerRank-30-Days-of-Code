b0c867554a34267ae62b66e9582de21de4e260db
Tim Neville <nikefootbag@gmail.com>
2017-06-26 22:47:00 +1000

    Started and completed Day 17 More Exceptions

---
+++ src/com/timneville/Day17_MoreExceptions.java
@@ -0,0 +1,42 @@
+package com.timneville;
+
+import java.util.*;
+import java.io.*;
+/**
+ * Created by timneville on 26/6/17.
+ */
+public class Day17_MoreExceptions {
+
+//Write your code here
+    class Calculator {
+        public int power(int n, int p) {
+            if (n < 0 || p < 0) {
+                throw new IllegalArgumentException("n and p should be non-negative");
+            }
+            return (int) Math.pow(n,p);
+        }
+}
+
+    class Solution{
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            int t = in.nextInt();
+            while (t-- > 0) {
+
+                int n = in.nextInt();
+                int p = in.nextInt();
+                Calculator myCalculator = new Calculator();
+                try {
+                    int ans = myCalculator.power(n, p);
+                    System.out.println(ans);
+                }
+                catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            in.close();
+        }
+    }
+}
